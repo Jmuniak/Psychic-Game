b8f6ee534da224a92f611c6468a96b8f258c606e
Jeremy Muniak <Jeremy.e.Muniak@gmail.com>
2019-07-14 10:00:53 -0400

    Wrote up the readMe, cleaned up some lines

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,20 +22,22 @@ function guessCheck() {
     let userGuess = Number(guessField.value);
     if (guessCount === 1) {
         guesses.textContent = 'Previous guesses: ';
-            }
+    }
     guesses.textContent += userGuess + ', ';
+
     // checking for a win condition
     if (userGuess === randomNumber) {
         lastResult.textContent = 'Wow you\'re a psychic! Are you a Jedi? You should play again!';
         lastResult.style.backgroundColor = 'lightgreen';
         lowOrHi.textContent = 'The Force is strong with you!';
         setGameOver();
-        // checking for guessCount for a loss condition
     }
+    // checking for guessCount for a loss condition
     else if (guessCount === 10) {
         lastResult.textContent = 'You\'re not a Jedi master yet. Try again Padawan!';
         setGameOver();
     }
+    // checking for more guessCoutns if they haven't won or lost yet
     else {
         lastResult.textContent = 'Incorrect!';
         lastResult.style.backgroundColor = 'orange';
@@ -46,12 +48,12 @@ function guessCheck() {
             lowOrHi.textContent = 'Last guess was too high!';
         }
     }
-    // Increase the guess Counter, minus the remainingGuesses, resets the guessing field.
+    // Increase the guess Counter, minus 1 from the remainingResults, resets the guessing field.
     guessCount++;
     remainingResults--;
     console.log(remainingResults);
     guessField.value = '';
-    // Focus sets the element as the active element in the current document. 
+    // Focus sets the element as the active element in the current document and changes the remainingResults text to match the new value. 
     guessField.focus();
     $(".remainingResults").text("Remaining Guesses: " + remainingResults);
 }
@@ -69,7 +71,7 @@ function setGameOver() {
 }
 // Reset Game function. I think this could be done differently. Specifically resetParas
 
-// Puts the guessCount back down to 1.
+// Puts the guessCount back down to 1, the remaining guesses back to 10 and updates the text to reflect 10 guesses left. 
 function resetGame() {
     guessCount = 1;
     remainingResults = 10;
@@ -79,7 +81,7 @@ function resetGame() {
     for (let i = 0; i < resetParas.length; i++) {
         resetParas[i].textContent = '';
     }
-    // Removes the reset button from our code.
+    // Removes the reset button
     resetButton.parentNode.removeChild(resetButton);
     // Enables the form elements, and empties and focuses the text field, ready for a new guess to be entered.
     guessField.disabled = false;
